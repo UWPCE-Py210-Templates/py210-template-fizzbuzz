4321a02e25c7bdbbe1123bb55a7eebe860276012
Natasha Aleksandrova <natasha.aleksandrova@zonarsystems.com>
2020-05-18 20:48:58 -0700

    initialize

---
+++ auto_test.py
@@ -0,0 +1,24 @@
+import contextlib, io
+import fizz_buzz
+
+f = io.StringIO()
+with contextlib.redirect_stdout(f):
+    fizz_buzz.fizz_buzz(10)
+
+output = f.getvalue()
+
+
+expected_output = \
+    """1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+"""
+
+assert output == expected_output
---
+++ fizz_buzz.py
@@ -0,0 +1,7 @@
+def fizz_buzz(n):
+    pass
+
+
+if __name__ == "__main__":
+    fizz_buzz(100)
+
